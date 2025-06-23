a1511f6e375ed910ced281f3065420bec7fc21c5
Aashish Parajuli <aasisjuli@gmail.com>
2025-06-23 08:41:14 +0545

    add: new welcome file

---
+++ welcome.py
@@ -0,0 +1,5 @@
+print("Hello world!!")
+print("This is a simple Python script.")
+print("Welcome to the Python script!")
+print("This script is designed to demonstrate a simple Python program.")   
+
