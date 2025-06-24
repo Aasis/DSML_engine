7fb21d55ad976863f98b946abf8e7ef2b3990df6
Aashish Parajuli <aasisjuli@gmail.com>
2025-06-24 17:58:00 +0545

    Add: Print statement

--- welcome.py
+++ welcome.py
@@ -3,3 +3,5 @@ print("This is a simple Python script.")
 print("Welcome to the Python script!")
 print("This script is designed to demonstrate a simple Python program.")   
 
+print(10+20 )
+print(" added in assignment branch ")
