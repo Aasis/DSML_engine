a22dd30cdefb42cac15c38386a8235a86932230a
Aashish Parajuli <aasisjuli@gmail.com>
2025-06-24 18:17:17 +0545

    2nd changes

--- welcome.py
+++ welcome.py
@@ -2,6 +2,7 @@ print("Hello world!!")
 print("This is a simple Python script.")
 print("Welcome to the Python script!")
 print("This script is designed to demonstrate a simple Python program.")   
-
 print(10+20 )
-print(" added in assignment branch ")
+print("Finally branch banyoooo")
+print(" lets see")
+
