82371c300bb71ac331ccbb80dd984b124d4402cb
Aashish Parajuli <aasisjuli@gmail.com>
2025-06-24 08:16:44 +0545

    added from branching

--- welcome.py
+++ welcome.py
@@ -1,5 +1,6 @@
 print("Hello world!!")
 print("This is a simple Python script.")
 print("Welcome to the Python script!")
-print("This script is designed to demonstrate a simple Python program.")   
-
+print("This script is designed to demonstrate a simple Python program.")
+a = 5+6
+print(a)
